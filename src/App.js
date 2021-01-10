f4b8898675bf7d1760ee46b022099734d8446223
olkhon <alexander.stapel@gmail.com>
2021-01-10 12:14:24 +0100

    pass different data types as properties

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Card from './components/card'
 function App() {
   return (
     <div className="App">
-      <Card text="default text" />
+      <Card text="default text" cold={true} trees={[23, 54, 46]} changeConsole={() => console.log('changed')} />
     </div>
   );
 }
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './styles.css';
 
-export default function Card ({text}) {
+
+export default function Card ({text, trees, changeConsole, cold}) {
+
+changeConsole();
     return (
         <div>
             {text}
+            {trees}
+            {cold}
         </div>
     )
 }
