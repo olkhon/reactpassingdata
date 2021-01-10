b41209dcc6cb90ef999583564a5dfd650ab0b307
olkhon <alexander.stapel@gmail.com>
2021-01-10 12:22:10 +0100

    initialize statevariable theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React, {useState} from 'react';
 import './App.css';
 import Card from './components/card'
 
 
 function App() {
+
+  const[theme, setTheme] = useState("dark");
+
   return (
-    <div className="App">
+    <div className="App" className={theme}>
       <Card text="default text" cold={true} trees={[23, 54, 46]} changeConsole={() => console.log('changed')} />
     </div>
   );
